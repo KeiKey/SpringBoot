83e4db6f924c756eb5f0a2ee71e786cc7aa5d027
keiselami <kei@aledia.ca>
2020-12-22 06:18:21 +0100

    Get student by id

--- src/main/java/com/example/demo/controller/StudentController.java
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -4,12 +4,10 @@ import com.example.demo.model.Student;
 import com.example.demo.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("api/v1/students")
@@ -27,6 +25,15 @@ public class StudentController {
     )
     public List<Student> getAllStudents() {
         return studentService.getAllStudents();
+    }
+
+    @RequestMapping(
+            method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_JSON_VALUE,
+            path = "{studentId}"
+    )
+    public Student getStudentById(@PathVariable("studentId")UUID studentId) {
+        return studentService.getStudentById(studentId);
     }
 
     @RequestMapping(
